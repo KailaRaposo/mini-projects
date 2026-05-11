5ccf19febe1c8c935d648e57e2d47858a3b8d85f
KailaRaposo <kailaraposo05@gmail.com>
2026-05-11 19:52:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function adicionar(valor){
+    document.getElementById("display").value += valor;
+}
+
+function limpar(){
+    document.getElementById("display").value = "";
+}
+
+function calcular(){
+    let display = document.getElementById("display");
+    
+    try {
+    let resultado = eval(display.value);
+    display.value = resultado;
+  } catch {
+    display.value = "Erro";
+  }
+}
